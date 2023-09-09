3d188bc731eb30c93f56871725bc2c42c10f46e9
Sergei <sergeiotoka@gmail.com>
2023-09-09 19:27:12 +0300

    First commit

---
+++ Diplom_finance_calculate/Diplom_finance_calculate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diplom_finance_calculate
+//
+//  Created by Sergei Otoka on 9.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
